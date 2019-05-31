d25207d64a41e1f1ceb48198b60aa3f036fe050e
Max Armstrong <maxarmstrong1995@gmail.com>
2019-05-31 12:14:24 -0400

    cleaned up create form

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -13,13 +13,41 @@
     <h1>Create Property</h1>
     <form method="POST" action="/properties/create" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="name" placeholder="name">
-        <textarea name="description" rows="10" placeholder="description"></textarea>
-        <input type="date" name="available_on" placeholder="available on">
-        <input type="text" name="rating" placeholder="rating">
-        <input type="text" name="price" placeholder="price">
-        <input type="text" name="country" placeholder="country">
-        <input type="file" name="photo" accept="image/*">
-        <button type="submit">Create</button>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input id="name" class="form-control" type="text" name="name" placeholder="name">
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea id="description" class="form-control" name="description" rows="10" placeholder="description"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="available">Available On</label>
+            <input id="available" class="form-control" type="date" name="available_on" placeholder="available on">
+        </div>
+
+        <div class="form-group">
+            <label for="rating">Rating</label>
+            <input id="rating" class="form-control" type="text" name="rating" placeholder="rating">
+        </div>
+
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input id="price" class="form-control" type="text" name="price" placeholder="price">
+        </div>
+
+        <div class="form-group">
+            <label for=""></label>
+            <input class="form-control" type="text" name="country" placeholder="country">
+        </div>
+
+        <div class="form-group">
+            <label for="photo">Photo</label>
+            <input id="photo" class="form-control" type="file" name="photo" accept="image/*">
+        </div>
+
+        <button class="btn btn-primary" type="submit">Create</button>
     </form>
 @endsection
